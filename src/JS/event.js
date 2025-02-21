9622e2f4699c916f9d0fdd20321ffcede6df6815
Vishesh-04 <visheshjindal04@gmail.com>
2025-02-21 17:08:25 +0530

    update in ui

--- src/JS/event.js
+++ src/JS/event.js
@@ -62,10 +62,10 @@ const closeModal = document.getElementById("close-modal");
 events.forEach((event) => {
   const eventCard = document.createElement("div");
   eventCard.className =
-    "relative overflow-hidden rounded-lg shadow-lg cursor-pointer group transition transform hover:scale-105";
+    "relative overflow-hidden rounded-lg shadow-lg cursor-pointer group transition transform hover:scale-105 duration-1000";
   eventCard.innerHTML = `
-    <img src="${event.images[0].src}" alt="${event.title}" class="font-[Barlow Condensed] w-full h-64 object-fit object-center group-hover:opacity-0 transition">
-    <div class="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition">
+    <img src="${event.images[0].src}" alt="${event.title}" class="font-[Barlow Condensed] w-full h-64 object-fit object-center group-hover:opacity-50 transition">
+    <div class="absolute inset-0 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition duration-1000 ease-in-out">
       <span class="text-white text-xl font-bold">${event.title}</span>
       <span class="text-white text-lg font-semibold bg-[#db0c26] px-4 py-2 rounded-lg mt-2">Explore</span>
     </div>
